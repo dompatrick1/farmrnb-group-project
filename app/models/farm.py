8263706a1ce3cf8f5a0138c897b0dac6c32ea53c
Dominic Patrick <dom.patrick92@gmail.com>
2021-04-25 18:30:10 -0400

    farm model

---
+++ app/models/farm.py
@@ -0,0 +1,14 @@
+from .db import db
+
+class Farm(db.Model):
+    __tablename__ = 'farms'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    type = db.Column(db.String(30), nullable=False)
+    job = db.Column(db.String(1000), nullable=False)
+    description = db.Column(db.String(1500), nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+    state = db.Column(db.String(100), nullable=False)
+    ownerId = db.Column(db.Integer)
+
