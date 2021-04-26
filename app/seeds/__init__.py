ae034819ba2be150fbd46e18390423c22c3f9b0b
Andrew Lee <andrewyujaylee@gmail.com>
2021-04-26 12:57:47 -0400

    added farms.py and seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,18 +1,25 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .farms import seed_farms, undo_farms
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
 seed_commands = AppGroup('seed')
 
 # Creates the `flask seed all` command
+
+
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_farms()
     # Add other seed functions here
 
 # Creates the `flask seed undo` command
+
+
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_farms()
     # Add other undo functions here
