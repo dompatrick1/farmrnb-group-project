2fa70dc1c1bf86142aa17b406aa2a1c161ff0eb7
cubOlson <cub.olson@gmail.com>
2021-04-26 17:31:23 -0400

    Initial farm display good

---
+++ react-app/src/store/farm.js
@@ -0,0 +1,56 @@
+//constraints
+
+const GET_FARMS = "farms/GET_FARMS";
+const GET_ONE_FARM = "farms/GET_ONE_FARM";
+
+const getFarms = (farms) => ({
+  type: GET_FARMS,
+  payload: farms
+})
+
+const getOneFarm = (farm) => ({
+  type: GET_ONE_FARM,
+  payload: farm
+})
+
+//thunks
+
+export const getFarmsThunk = () => async (dispatch)  => {
+  const response = await fetch('/api/farms')
+  if (!response.ok) {
+    throw response
+  }
+  const farms = await response.json();
+  dispatch(getFarms(farms))
+}
+
+export const getOneFarmThunk = (id) => async (dispatch)  => {
+    const response = await fetch(`/api/farms/${id}`)
+    if (!response.ok) {
+      throw response
+    }
+    const farm = await response.json();
+    dispatch(getOneFarm(farm))
+  }
+
+const initialState = {}
+
+const farmsReducer = (farms = initialState, action) => {
+    switch(action.type){
+        case GET_FARMS:
+            const farmsPayload = action.payload
+            const newFarms = {};
+            for (const farm of farmsPayload) {
+                newFarms[farm.id] = farm
+            }
+            return newFarms;
+        case GET_ONE_FARM:
+            const farmPayload = action.payload
+            return farmPayload
+        default:
+            return farms;
+    }
+}
+
+export default farmsReducer;
+
--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,10 +1,12 @@
 import {createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from 'redux-thunk';
 import session from './session'
+import farmReducer from './farm'
 
 
 const rootReducer = combineReducers({
-    session
+    session,
+    farms : farmReducer
 });
 
 let enhancer;
