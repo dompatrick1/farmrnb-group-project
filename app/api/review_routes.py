5b0359fcbd5cc84982b2a16ac03e5cabd89ee661
Dominic Patrick <dom.patrick92@gmail.com>
2021-04-26 16:19:49 -0400

    reservation and reviews routes

---
+++ app/api/review_routes.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, jsonify
+from app.models import Review
+
+review_routes = Blueprint('reviews', __name__)
+
+@review_routes.route('/')
+def all_reviews():
+    reviews = Review.query.all()
+    return {"reviews": [review.to_dict() for review in reviews]}
+
+
+@review_routes.route('/<int:id>')
+def get_review(id):
+    reviews = Review.query.filter(Review.farmId == id).all()
+    return {"reviews": [review.to_dict() for review in reviews]}
